60018f009f1b6c6d553841847658bcf75a9156a4
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-01-25 22:17:53 +0100

    ft--dependencies,sanity,web3rd added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const supportedChainIds = [4]
+const connectors = {
+  injected: {},
+}
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
